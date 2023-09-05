00c20e0c42be76012606375c64dcf1fd66e6722d
brian <blobo@redhat.com>
2023-09-05 10:54:41 -0300

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 import { ExamplesModule } from './examples/examples.module';
 
+import { NouisliderComponent } from 'ng2-nouislider';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent
+        NavbarComponent,
     ],
     imports: [
         BrowserAnimationsModule,
--- src/app/examples/examples.module.ts
+++ src/app/examples/examples.module.ts
@@ -1,32 +1,28 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NouisliderModule } from 'ng2-nouislider';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
-import { AgmCoreModule } from '@agm/core';
+import { JwBootstrapSwitchNg2Module } from '@servoy/jw-bootstrap-switch-ng2';
+import { NouisliderModule } from 'ng2-nouislider';
 
+import { ExamplesComponent } from './examples.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ExamplesComponent } from './examples.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         NgbModule,
-        NouisliderModule,
         JwBootstrapSwitchNg2Module,
-        AgmCoreModule.forRoot({
-            apiKey: 'YOUR_KEY_HERE'
-        })
+        NouisliderModule
     ],
     declarations: [
         LandingComponent,
         LoginComponent,
         ExamplesComponent,
-        ProfileComponent
+        ProfileComponent,
     ]
 })
 export class ExamplesModule { }
